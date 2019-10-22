e4a56bb684f507cb6ad3122167987af312a8f34b
Rammus Xu <comte_ken@hotmail.com>
2019-10-22 11:58:23 +0800

    Add gcloud/login

---
+++ gcloud/login/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+echo "$GCLOUD_AUTH" | base64 -d > gcloud.json
+
+gcloud auth activate-service-account --key-file=gcloud.json
+
+# https://cloud.google.com/container-registry/docs/advanced-authentication
+cat gcloud.json | docker login -u _json_key --password-stdin https://asia.gcr.io
+
+sh -c "$*"

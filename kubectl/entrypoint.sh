2613a583a5c05e3b47516075b14ff226a75cb7b0
Rammus Xu <comte_ken@hotmail.com>
2020-05-15 11:37:16 +0800

    Add context arg

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-
 set -e
 
 echo "$KUBE_CONFIG" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
+
+if [[ "$CONTEXT" ]]; then
+    kubectl config use-context $CONTEXT
+fi
 
 bash -c "$*"

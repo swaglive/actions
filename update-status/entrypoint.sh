77482d7d68da6b241b4c30e1274a6806e92a6679
Rammus Xu <comte_ken@hotmail.com>
2020-06-04 12:08:26 +0800

    Add context tag

--- update-status/entrypoint.sh
+++ update-status/entrypoint.sh
@@ -3,12 +3,18 @@
 if [[ "$GITHUB_EVENT_NAME" = "pull_request" ]]; then
     COMMIT_SHA=$INPUT_PULL_SHA
 else
-    COMMIT_SHA=$INPUT_PUSH_SHA
+    COMMIT_SHA=$GITHUB_SHA
+fi
+
+if [[ $GITHUB_REF == *"refs/tags"* ]]; then
+    TAG=${GITHUB_REF/refs\/tags\//}
+else
+    TAG=$COMMIT_SHA
 fi
 
 http POST https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${COMMIT_SHA}/statuses \
     "Authorization: token ${INPUT_AUTH_TOKEN}" \
     state=${INPUT_STATE:-success} \
-    context="update-status deploy:${INPUT_DEPLOY_TO}" \
+    context="update-status deploy:${INPUT_DEPLOY_TO} tag:${TAG}" \
     target_url=https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID} \
     --ignore-stdin -v

b3e242e62db8b96b9e97401019d3b817f5066523
Rammus Xu <comte_ken@hotmail.com>
2020-06-22 18:12:43 +0800

    Fix env

--- flexible-env/entrypoint.sh
+++ flexible-env/entrypoint.sh
@@ -2,22 +2,26 @@
 set -e
 
 if [[ "$GITHUB_EVENT_NAME" = "pull_request" ]]; then
-    echo "::set-env name=FLEX_GIT_SHA::$INPUT_PULL_SHA"
+    FLEX_GIT_SHA="$INPUT_PULL_SHA"
 else
-    echo "::set-env name=FLEX_GIT_SHA::$GITHUB_SHA"
+    FLEX_GIT_SHA="$GITHUB_SHA"
 fi
 
 if [ $GITHUB_HEAD_REF ]; then
-    echo "::set-env name=FLEX_GIT_REF::$GITHUB_HEAD_REF"
+    FLEX_GIT_REF="$GITHUB_HEAD_REF"
 else
-    echo "::set-env name=FLEX_GIT_REF::$GITHUB_REF"
+    FLEX_GIT_REF="$GITHUB_REF"
 fi
 
 if echo $GITHUB_REF | grep -q "refs/tags" ; then
     echo "::set-env name=FLEX_GIT_TAG::${GITHUB_REF/refs\/tags\//}"
 fi
 
-# Shorten
+# Export env
+echo "::set-env name=FLEX_GIT_SHA::$FLEX_GIT_SHA"
+echo "::set-env name=FLEX_GIT_REF::$FLEX_GIT_REF"
+
+## Shorten
 echo "::set-env name=FLEX_GIT_SHA_SHORT::${FLEX_GIT_SHA:0:7}"
 echo "::set-env name=FLEX_GIT_REF_SHORT::${FLEX_GIT_REF#refs/(heads|tags)/}"
 echo "::set-env name=FLEX_GIT_REF_FORMAT::${FLEX_GIT_REF_SHORT//[^a-z0-9A-Z]/-}"

118e02f506bed76782a6a407ed2058563035988f
Rammus Xu <comte_ken@hotmail.com>
2020-06-30 17:48:07 +0800

    Fix python2

--- gcloud/login/entrypoint.sh
+++ gcloud/login/entrypoint.sh
@@ -8,4 +8,4 @@ gcloud auth activate-service-account --key-file=gcloud.json
 # https://cloud.google.com/container-registry/docs/advanced-authentication
 cat gcloud.json | docker login -u _json_key --password-stdin https://asia.gcr.io
 
-bash -c "$*"
+sh -c "$*"
